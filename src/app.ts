47ff4c4633306c3eb5cfe7f4f4bf51cde3730c25
Vector-ops <sumith2827@gmail.com>
2024-08-26 13:18:45 +0530

    feat: implement logger in server

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import dotenv from "dotenv";
 import express, { Express } from "express";
+import prisma from "./lib/prisma";
+import { logger, morganMiddleware } from "./utils/logger";
 
 dotenv.config();
 
@@ -8,12 +10,15 @@ const app: Express = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(morganMiddleware);
 
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
+	logger.info(`Server running on port ${PORT}`);
 });
 
 process.on("SIGINT", async () => {
 	await prisma.$disconnect();
+	logger.info("SIGINT signal received. Stopping server.");
 	process.exit();
 });
