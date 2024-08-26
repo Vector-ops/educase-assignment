824ce0c9018fe04733c6d4025ee9d9a885f67ef0
Vector-ops <sumith2827@gmail.com>
2024-08-26 16:43:27 +0530

    feat: add routing to the server

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import dotenv from "dotenv";
 import express, { Express } from "express";
 import prisma from "./lib/prisma";
+import { errorHandler } from "./middleware/error-handler";
+import { notFound } from "./middleware/not-found";
+import router from "./routers/school.router";
 import { logger, morganMiddleware } from "./utils/logger";
 
 dotenv.config();
@@ -11,6 +14,9 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(morganMiddleware);
+app.use("/api/v1", router);
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
